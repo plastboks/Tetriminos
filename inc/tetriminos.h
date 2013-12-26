9aefa394dc49e47527570d00afb6d60f80687eac
Alexander Skjolden <alex@plastboks.net>
2013-12-26 11:36:25 +0100

    some empty sourcefiles
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ inc/tetriminos.h
@@ -0,0 +1,28 @@
+/*
+ * Tetriminos header file
+ *
+ * @filename: tetriminos.h
+ *
+ * @version: 0.0.1
+ *
+ * @date: 2013-12-26
+ *
+ * @description: Tetriminos main header file.
+ *
+ * This file is part of Tetriminos.
+ *
+ * Tetriminos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tetriminos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tetriminos.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
