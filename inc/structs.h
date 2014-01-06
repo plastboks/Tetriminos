0616d7599116f766fb9907ca2adedd85ae9b8377
Alexander Skjolden <alex@plastboks.net>
2014-01-06 11:23:45 +0100

    game box
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- inc/structs.h
+++ inc/structs.h
@@ -34,11 +34,13 @@ struct {
     int splash_box_size[2];     /* splash box dimensions */
     int menu_box_size[2];       /* menu box dimensions */
     int about_box_size[2];      /* about box dimensions */
+    int game_box_size[2];      /* about box dimensions */
 } c = {
     /* set value */
     {9, 54},    /* splash box */
     {14, 25},   /* menu box */
-    {20, 44}    /* about box */
+    {20, 44},   /* about box */
+    {20, 44}    /* game box */
 };
 
 struct {
