bcdb40b0cf2aecad50949a2e269acedab05bf60b
Alexander Skjolden <alex@plastboks.net>
2014-01-11 17:38:21 +0100

    more color pairs
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/screens.c
+++ src/screens.c
@@ -231,8 +231,13 @@ void screen_colors()
     start_color();
 
     init_pair(1, COLOR_RED, COLOR_BLACK);
-    init_pair(2, COLOR_CYAN, COLOR_BLACK);
-    init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_BLUE, COLOR_BLACK);
+    init_pair(4, COLOR_CYAN, COLOR_BLACK);
+    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(6, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(15, COLOR_WHITE, COLOR_BLACK);
+    init_pair(16, COLOR_BLACK, COLOR_BLACK);
 }
 
 /**
